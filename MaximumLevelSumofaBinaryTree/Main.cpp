cbb8cb73c519425b429b0f3d446700d114171cbd
OguzSahin58 <oguz.sahin.20742@ozu.edu.tr>
2026-01-08 18:17:23 +0300

    1161. Maximum Level Sum of a Binary Tree

---
+++ MaximumLevelSumofaBinaryTree/Main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <queue>
+#include <limits>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int maxLevelSum(TreeNode* root) {
+        int maxSum = INT_MIN;
+        int ans = 0, level = 0;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            level++;
+            int sumAtCurrentLevel = 0;
+            // Iterate over all the nodes in the current level.
+            for (int sz = q.size(); sz > 0; --sz) {
+                TreeNode* node = q.front();
+                q.pop();
+                sumAtCurrentLevel += node->val;
+
+                if (node->left != nullptr) {
+                    q.push(node->left);
+                }
+                if (node->right != nullptr) {
+                    q.push(node->right);
+                }
+            }
+
+            if (maxSum < sumAtCurrentLevel) {
+                maxSum = sumAtCurrentLevel;
+                ans = level;
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    Solution solution;
+
+    // Construct tree for [1,7,0,7,-8,null,null]
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(7);
+    root->right = new TreeNode(0);
+    root->left->left = new TreeNode(7);
+    root->left->right = new TreeNode(-8);
+    // right children are null
+
+    cout << solution.maxLevelSum(root) << endl; // Expected 2
+
+    // Note: skipping deletes for brevity in this small test
+    return 0;
+}
